870caa4021f5bc6bbb8a1f030fc90980dc4fa2f8
kolaente <k@knt.li>
2021-02-21 14:00:01 +0100

    Hide notifications popup when clicking away from it

--- src/components/notifications/notifications.vue
+++ src/components/notifications/notifications.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="notifications">
-		<a @click="showNotifications = !showNotifications" class="trigger">
+		<a @click.stop="showNotifications = !showNotifications" class="trigger">
 			<icon icon="bell"/>
 		</a>
 
 		<transition name="fade">
-			<div class="notifications-list" v-if="showNotifications">
+			<div class="notifications-list" v-if="showNotifications" ref="popup">
 				<span class="head">Notifications</span>
 				<div class="single-notification" v-for="n in notifications" :key="n.id">
 					{{ n.name }}
@@ -18,9 +18,13 @@
 
 <script>
 import NotificationService from '@/services/notification'
+import User from '@/components/misc/user'
+import names from '@/models/notificationNames.json'
+import {closeWhenClickedOutside} from '@/helpers/closeWhenClickedOutside'
 
 export default {
 	name: 'notifications',
+	components: {User},
 	data() {
 		return {
 			notificationService: NotificationService,
@@ -33,8 +37,17 @@ export default {
 	},
 	mounted() {
 		this.loadNotifications()
+		document.addEventListener('click', this.hidePopup)
+	},
+	beforeDestroy() {
+		document.removeEventListener('click', this.hidePopup)
 	},
 	methods: {
+		hidePopup(e) {
+			if (this.showNotifications) {
+				closeWhenClickedOutside(e, this.$refs.popup, () => this.showNotifications = false)
+			}
+		},
 		loadNotifications() {
 			this.notificationService.getAll()
 				.then(r => {
