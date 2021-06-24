d86ba0a48f2ff218fad533e842608609c854b96b
kolaente <k@knt.li>
2021-06-24 18:26:08 +0200

    Add date parsing for the usual dates

--- src/helpers/parseTaskText.js
+++ src/helpers/parseTaskText.js
@@ -1,3 +1,7 @@
+import {calculateDayInterval} from './time/calculateDayInterval'
+import {calculateNearestHours} from './time/calculateNearestHours'
+import {replaceAll} from './replaceAll'
+
 /**
  * Parses task text for dates, assignees, labels, lists, priorities and returns an object with all found intents.
  *
@@ -6,6 +10,7 @@
 export const parseTaskText = text => {
 	const result = {
 		text: text,
+		date: null,
 		labels: [],
 		list: null,
 		priority: null,
@@ -13,20 +18,74 @@ export const parseTaskText = text => {
 	}
 
 	result.labels = parseLabels(text)
+	const {newText, date} = parseDate(text)
+	result.text = newText
+	result.date = date
 
-	result.labels.forEach(l => {
-		result.text = result.text
-			.replace(`~'${l}' `, '')
-			.replace(`~'${l}'`, '')
-			.replace(`~"${l}" `, '')
-			.replace(`~"${l}"`, '')
-			.replace(`~${l} `, '')
-			.replace(`~${l}`, '')
-	})
+	return cleanupResult(result)
+}
 
-	result.text = result.text.trim()
+const parseDate = text => {
+	const lowerText = text.toLowerCase()
 
-	return result
+	if (lowerText.includes('today')) {
+		return {
+			newText: replaceAll(text, 'today', ''),
+			date: getDateFromInterval(calculateDayInterval('today')),
+		}
+	}
+	if (lowerText.includes('tomorrow')) {
+		return {
+			newText: replaceAll(text, 'tomorrow', ''),
+			date: getDateFromInterval(calculateDayInterval('tomorrow')),
+		}
+	}
+	if (lowerText.includes('next monday')) {
+		return {
+			newText: replaceAll(text, 'next monday', ''),
+			date: getDateFromInterval(calculateDayInterval('nextMonday')),
+		}
+	}
+	if (lowerText.includes('this weekend')) {
+		return {
+			newText: replaceAll(text, 'this weekend', ''),
+			date: getDateFromInterval(calculateDayInterval('thisWeekend')),
+		}
+	}
+	if (lowerText.includes('later this week')) {
+		return {
+			newText: replaceAll(text, 'later this week', ''),
+			date: getDateFromInterval(calculateDayInterval('laterThisWeek')),
+		}
+	}
+	if (lowerText.includes('later next week')) {
+		return {
+			newText: replaceAll(text, 'later next week', ''),
+			date: getDateFromInterval(calculateDayInterval('laterNextWeek')),
+		}
+	}
+	if (lowerText.includes('next week')) {
+		return {
+			newText: replaceAll(text, 'next week', ''),
+			date: getDateFromInterval(calculateDayInterval('nextWeek')),
+		}
+	}
+
+
+	return {
+		newText: text,
+		date: null,
+	}
+}
+
+const getDateFromInterval = interval => {
+	const newDate = new Date()
+	newDate.setDate(newDate.getDate() + interval)
+	newDate.setHours(calculateNearestHours(newDate))
+	newDate.setMinutes(0)
+	newDate.setSeconds(0)
+
+	return newDate
 }
 
 const parseLabels = text => {
@@ -52,4 +111,20 @@ const parseLabels = text => {
 	})
 
 	return Array.from(new Set(labels))
+}
+
+const cleanupResult = result => {
+	result.labels.forEach(l => {
+		result.text = result.text
+			.replace(`~'${l}' `, '')
+			.replace(`~'${l}'`, '')
+			.replace(`~"${l}" `, '')
+			.replace(`~"${l}"`, '')
+			.replace(`~${l} `, '')
+			.replace(`~${l}`, '')
+	})
+
+	result.text = result.text.trim()
+
+	return result
 }
--- src/helpers/parseTaskText.test.js
+++ src/helpers/parseTaskText.test.js
@@ -1,11 +1,94 @@
 import {parseTaskText} from './parseTaskText'
+import {calculateDayInterval} from './time/calculateDayInterval'
 
 describe('Parse Task Text', () => {
 	it('should return text with no intents as is', () => {
 		expect(parseTaskText('Lorem Ipsum').text).toBe('Lorem Ipsum')
 	})
 
-	describe('labels', () => {
+	describe('Date Parsing', () => {
+		it('should recognize today', () => {
+			const result = parseTaskText('Lorem Ipsum today')
+
+			expect(result.text).toBe('Lorem Ipsum')
+			const now = new Date()
+			expect(result.date.getFullYear()).toBe(now.getFullYear())
+			expect(result.date.getMonth()).toBe(now.getMonth())
+			expect(result.date.getDate()).toBe(now.getDate())
+		})
+		it('should recognize tomorrow', () => {
+			const result = parseTaskText('Lorem Ipsum tomorrow')
+
+			expect(result.text).toBe('Lorem Ipsum')
+			const tomorrow = new Date()
+			tomorrow.setDate(tomorrow.getDate() + 1)
+			expect(result.date.getFullYear()).toBe(tomorrow.getFullYear())
+			expect(result.date.getMonth()).toBe(tomorrow.getMonth())
+			expect(result.date.getDate()).toBe(tomorrow.getDate())
+		})
+		it('should recognize next monday', () => {
+			const result = parseTaskText('Lorem Ipsum next monday')
+
+			const untilNextMonday = calculateDayInterval('nextMonday')
+
+			expect(result.text).toBe('Lorem Ipsum')
+			const nextMonday = new Date()
+			nextMonday.setDate(nextMonday.getDate() + untilNextMonday)
+			expect(result.date.getFullYear()).toBe(nextMonday.getFullYear())
+			expect(result.date.getMonth()).toBe(nextMonday.getMonth())
+			expect(result.date.getDate()).toBe(nextMonday.getDate())
+		})
+		it('should recognize this weekend', () => {
+			const result = parseTaskText('Lorem Ipsum this weekend')
+
+			const untilThisWeekend = calculateDayInterval('thisWeekend')
+
+			expect(result.text).toBe('Lorem Ipsum')
+			const thisWeekend = new Date()
+			thisWeekend.setDate(thisWeekend.getDate() + untilThisWeekend)
+			expect(result.date.getFullYear()).toBe(thisWeekend.getFullYear())
+			expect(result.date.getMonth()).toBe(thisWeekend.getMonth())
+			expect(result.date.getDate()).toBe(thisWeekend.getDate())
+		})
+		it('should recognize later this week', () => {
+			const result = parseTaskText('Lorem Ipsum later this week')
+
+			const untilLaterThisWeek = calculateDayInterval('laterThisWeek')
+
+			expect(result.text).toBe('Lorem Ipsum')
+			const laterThisWeek = new Date()
+			laterThisWeek.setDate(laterThisWeek.getDate() + untilLaterThisWeek)
+			expect(result.date.getFullYear()).toBe(laterThisWeek.getFullYear())
+			expect(result.date.getMonth()).toBe(laterThisWeek.getMonth())
+			expect(result.date.getDate()).toBe(laterThisWeek.getDate())
+		})
+		it('should recognize later next week', () => {
+			const result = parseTaskText('Lorem Ipsum later next week')
+
+			const untilLaterNextWeek = calculateDayInterval('laterNextWeek')
+
+			expect(result.text).toBe('Lorem Ipsum')
+			const laterNextWeek = new Date()
+			laterNextWeek.setDate(laterNextWeek.getDate() + untilLaterNextWeek)
+			expect(result.date.getFullYear()).toBe(laterNextWeek.getFullYear())
+			expect(result.date.getMonth()).toBe(laterNextWeek.getMonth())
+			expect(result.date.getDate()).toBe(laterNextWeek.getDate())
+		})
+		it('should recognize next week', () => {
+			const result = parseTaskText('Lorem Ipsum next week')
+
+			const untilNextWeek = calculateDayInterval('nextWeek')
+
+			expect(result.text).toBe('Lorem Ipsum')
+			const nextWeek = new Date()
+			nextWeek.setDate(nextWeek.getDate() + untilNextWeek)
+			expect(result.date.getFullYear()).toBe(nextWeek.getFullYear())
+			expect(result.date.getMonth()).toBe(nextWeek.getMonth())
+			expect(result.date.getDate()).toBe(nextWeek.getDate())
+		})
+	})
+
+	describe('Labels', () => {
 		it('should parse labels', () => {
 			const result = parseTaskText('Lorem Ipsum ~label1 ~label2')
 
