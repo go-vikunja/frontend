e59fa416003f38291e6b6f8ee7adbc62d5028e7f
kolaente <k@knt.li>
2021-11-16 22:41:40 +0100

    feat: add tests for finding the api url

---
+++ src/helpers/checkAndSetApiUrl.test.ts
@@ -0,0 +1,8 @@
+describe('Find the correct api url', () => {
+	// TODO: To check:
+	// /api/v1
+	// / (should append api suffix)
+	// :3456/api/v1
+	// :3456/ (should append api suffix)
+	// + Everything with http and https -> start with the current protocol, then try the other one
+})
