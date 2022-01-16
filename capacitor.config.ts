f52dd5e772da561735248975db3543c03bb858a6
kolaente <k@knt.li>
2022-01-16 16:38:37 +0100

    chore: move to ts configuration for capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+	appId: 'io.vikunja.app',
+	appName: 'Vikunja',
+	webDir: 'dist',
+	bundledWebRuntime: false,
+}
+
+export default config
